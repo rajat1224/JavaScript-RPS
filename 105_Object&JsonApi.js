38e4510de451df08b2395f5eac38fa993068e234
Rajat Pratap Singh <rajputrajat1224@gmail.com>
2024-04-12 00:41:29 +0530

    Update 105_Object&JsonApi.js
    changed

--- 105_Object&JsonApi.js
+++ 105_Object&JsonApi.js
@@ -1,1 +1,12 @@
 //Object de-structure and JSON API
+
+const obj={
+    user:"Rajat",
+    UserDetails:"ggggg"
+}
+
+console.log(obj);
+const {UserDetails:ins}=obj;   //called de-structuring
+console.log(ins);
+console.log();
+console.log();
