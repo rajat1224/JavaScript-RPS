77cfe746db15300b5b2d3ced5a7fa69399aecba5
Rajat Pratap Singh <rajputrajat1224@gmail.com>
2024-04-12 00:56:30 +0530

    Update 105_Object&JsonApi.js
    come introduction of json

--- 105_Object&JsonApi.js
+++ 105_Object&JsonApi.js
@@ -10,3 +10,13 @@ const {UserDetails:ins}=obj;   //called de-structuring
 console.log(ins);
 console.log();
 console.log();
+
+//let fun=({Order})=>{}  //{abc} here it is destructuring
+
+                                                  //Json
+                                            //  {
+                                            //     "name":"rajat",
+                                            //     "age":24,...
+                                            //  }
+                                                  //or
+                                            //[{},{},{}]
